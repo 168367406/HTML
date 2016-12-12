2b4eadbaae21825243b8f96b08cd264292a80ec0
168367406@qq.com <yanzi123>
2016-12-12 17:23:39 +0800

    1
    Signed-off-by: 168367406@qq.com <yanzi123>

--- Hadmin/js/content.js
+++ Hadmin/js/content.js
@@ -18,7 +18,8 @@
 
 //判断当前页面是否在iframe中
 if (top == this) {
-    var gohome = '<div class="gohome"><a class="animated bounceInUp" href="index.html?v=4.0" title="返回首页"><i class="fa fa-home"></i></a></div>';
+    alert("sadfaf");
+    var gohome = '<div class="gohome"><a class="animated bounceInUp" href="index.html" title="返回首页"><i class="fa fa-home"></i></a></div>';
     $('body').append(gohome);
 }
 

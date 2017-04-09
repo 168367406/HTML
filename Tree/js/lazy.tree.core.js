412e9fd4676c90dece6afdea39447059353f05db
lazy <168367406@qq.com>
2017-04-10 00:02:07 +0800

    根据jQuery插件编写规范指定了模板，准备开始写了，哈哈

--- Tree/js/lazy.tree.core.js
+++ Tree/js/lazy.tree.core.js
@@ -6,79 +6,52 @@
  * Version: 0.1
  * Info: Tree核心js工具类
  */
-;(function ($, window, document, undefined) {
-    var defaultSetting = {
-        'color': 'red',
-        'fontSize': '12px',
-        'textDecoration': 'none'
-    };
-    //定义Beautifier的构造函数
-    var LazyTree = function (ele, opt) {
-        this.$element = ele,
-            this.defaults = defaultSetting,
-            this.options = $.extend({}, this.defaults, opt)
-    }
-    //定义Beautifier的方法
-    LazyTree.prototype = {
-        init: function () {
-            return $(this.$element).css({
-                'color': this.options.color,
-                'fontSize': this.options.fontSize,
-                'textDecoration': this.options.textDecoration
-            });
+(function ($, window, document, undefined) {
+    var setting = {}, obj = null;
+
+    var methods = {
+        init: function (options, data) {
+            if (data==null)
+                console.log(data);
+            var _html = "";
+            _html += "<ul>";
+            _html += "<li>";
+            _html += "<i class='fa fa-chevron-down'></i>";
+            _html += "<a href='javascript:;'><i class='fa fa-folder-open'></i>我的邮箱</a>";
+
+            _html += "</li>";
+            _html += "</ul>";
+            alert("aa");
+            return this.append(_html);
+            function test() {
+                alert("11   ");
+            }
+        },
+        show: function () {
+            // is
+        },
+        hide: function () {
+            // good
+        },
+        update: function (content) {
+            // !!!
         }
+    };
+
+    $.fn.lazyTree = function (method) {
+
+        // 方法调用
+        if (methods[method]) {
+            return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+        } else if (typeof method === 'object' || !method) {
+            return methods.init.apply(this, arguments);
+        } else {
+            $.error('Method ' + method + ' does not exist on jQuery.lazyTree');
+        }
+
+    };
+    $.fn.lazyTree.test=function (obj) {
+        var o=obj;
+        return obj.html();
     }
-    //在插件中使用Beautifier对象
-    $.fn.lazyTree = function (options) {
-        // var lazyTree = new LazyTree(this, options);
-        // return lazyTree.init();
-    };
-    $.fn.lazyTree.init = function (options) {
-        var lazyTree = new LazyTree(this, options);
-        return lazyTree.init();
-    };
-    $.fn.lazyTree.defaultSettings = {
-        color: ""
-    };
-
 })(jQuery, window, document);
-
-
-(function ($) {
-    // 插件的定义
-    $.fn.hilight = function (options) {
-        debug(this);
-        // build main options before element iteration
-        var opts = $.extend({}, $.fn.hilight.defaults, options);
-        // iterate and reformat each matched element
-        return this.each(function () {
-            $this = $(this);
-            // build element specific options
-            var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
-            // update element styles
-            $this.css({
-                backgroundColor: o.background,
-                color: o.foreground
-            });
-            var markup = $this.html();
-            // call our format function
-            markup = $.fn.hilight.format(markup);
-            $this.html(markup);
-        });
-    };
-    // 私有函数：debugging
-    function debug($obj) {
-        if (window.console && window.console.log)
-            window.console.log('hilight selection count: ' + $obj.size());
-    };
-    // 定义暴露format函数
-    $.fn.hilight.format = function (txt) {
-        return '<strong>' + txt + '</strong>';
-    };
-    // 插件的defaults
-    $.fn.hilight.defaults = {
-        foreground: 'red',
-        background: 'yellow'
-    };
-// 闭包结束
-})(jQuery);
